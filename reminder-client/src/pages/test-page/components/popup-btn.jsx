c036df9712155edb46a6dc2041051e625ed3bdb0
Harsh Kashiwal <kashiwalharsh1234@gmail.com>
2023-12-20 18:42:53 +0530

    added modal on click of a button

--- reminder-client/src/pages/test-page/components/popup-btn.jsx
+++ reminder-client/src/pages/test-page/components/popup-btn.jsx
@@ -1,46 +1,54 @@
 import React, { useState } from 'react';
 import { Button, Modal } from '@mui/material';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-
-import './popup-btn.css'; // Import the corresponding CSS file
+import './popup-btn.css';
 
 const PopupBTN = () => {
-  const [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(false);
 
-  const handleOpen = () => {
-    setOpen(true);
-  };
+    const handleOpen = () => {
+        setOpen(true);
+    };
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+    const handleClose = () => {
+        setOpen(false);
+    };
 
-  return (
-    <div className="reminder-container">
-      <Button
-        variant="contained"
-        color="primary"
-        startIcon={<AddCircleOutlineIcon />}
-        onClick={handleOpen}
-      >
-        Add Reminder
-      </Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="reminder-modal-title"
-        aria-describedby="reminder-modal-description"
-      >
-        <div className="reminder-modal">
-          <h2 id="reminder-modal-title">Random Text Here</h2>
-          <p id="reminder-modal-description">
-            This is some random text for the modal.
-          </p>
-          <Button onClick={handleClose}>Close</Button>
+    return (
+        <div className="reminder-container">
+            <Button
+                variant="contained"
+                color="primary"
+                startIcon={<AddCircleOutlineIcon />}
+                onClick={handleOpen}
+            >
+                Add Reminder
+            </Button>
+            <Modal
+                open={open}
+                onClose={handleClose}
+            >
+                <div className="reminder-modal">
+                    <h2 id="reminder-modal-title">Remind Me</h2>
+                    <input type='text' placeholder='Reminder note here...'></input>
+                    {/* <input type='text' placeholder='Reminder note here...' value={reminderMsg} onChange={e => setReminderMsg(e.target.value)}></input> */}
+                    {/* <DateTimePicker
+                        value={remindAt}
+                        onChange={setRemindAt}
+                        minDate={new Date()}
+                        minutePlaceholder="mm"
+                        hourPlaceholder="hh"
+                        dayPlaceholder="DD"
+                        monthPlaceholder="MM"
+                        yearPlaceholder="YYYY"
+                    /> */}
+                    <div className='submit-btn'>Add Reminder</div>
+                    {/* <div className='submit-btn' onClick={addReminder}>Add Reminder</div> */}
+                    <Button onClick={handleClose}>Close</Button>
+                </div>
+            </Modal>
         </div>
-      </Modal>
-    </div>
-  );
+    );
 };
 
 export default PopupBTN;
