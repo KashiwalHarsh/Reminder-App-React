fd7b370226e5d77571ce5699fe8a003654f6ea33
Harsh Kashiwal <kashiwalharsh1234@gmail.com>
2023-12-21 19:12:00 +0530

    did some more tweeks

--- reminder-client/src/pages/test-page/components/right-section.jsx
+++ reminder-client/src/pages/test-page/components/right-section.jsx
@@ -8,26 +8,26 @@ const RightSection = () => {
             <div className="reminder-card">
               <h2>Reminder Note Demo that complete this work or call someone and check mail</h2>
               <h3>Remind me at : </h3>
-              <span>Time to be displayed</span>
+              <span> Time to be displayed</span>
               <h3>Reminder Status : </h3>
               <span style={{color:"orange",fontWeight:'bold'}}> Pending</span>
-              <div className="button"><DeleteIcon fontSize='small' color='secondary'/>&nbsp; Delete</div>
+              <div className="button"><DeleteIcon className='delete-icon'/>&nbsp; Delete</div>
             </div>
             <div className="reminder-card">
               <h2>Check whatsapp and instagram scroll for 15 minutes</h2>
               <h3>Remind me at : </h3>
-              <span>Time to be displayed</span>
+              <span> Time to be displayed</span>
               <h3>Reminder Status : </h3>
               <span style={{color:"yellowgreen",fontWeight:'bold'}}> Sent</span>
-              <div className="button"><DeleteIcon fontSize='small' color='secondary'/>&nbsp; Delete</div>
+              <div className="button"><DeleteIcon className='delete-icon'/>&nbsp; Delete</div>
             </div>
             <div className="reminder-card">
               <h2>Reminder Note Demo that complete this work or call someone and check mail</h2>
               <h3>Remind me at : </h3>
-              <span>Time to be displayed</span>
+              <span> Time to be displayed</span>
               <h3>Reminder Status : </h3>
               <span style={{color:"yellowgreen",fontWeight:'bold'}}> Sent</span>
-              <div className="button"><DeleteIcon fontSize='small' color='secondary'/>&nbsp; Delete</div>
+              <div className="button"><DeleteIcon className='delete-icon'/>&nbsp; Delete</div>
             </div>
             
     </div>
