faf60532dbd8d14fe141ab863a102559fb27d116
Harsh Kashiwal <kashiwalharsh1234@gmail.com>
2023-12-26 15:01:24 +0530

    added local storage for remembering the last theme mode

--- reminder-client/src/pages/test-page/context/ThemeContextProvider.jsx
+++ reminder-client/src/pages/test-page/context/ThemeContextProvider.jsx
@@ -1,8 +1,10 @@
-import React,{useState} from 'react'
+import React from 'react'
 import ThemeContext from './ThemeContext'
+import useLocalStorage from 'use-local-storage'
 
 const ThemeContextProvider =({children})=>{
-    const [darkTheme,setDarkTheme] = useState(true)
+    //using localstorage to store the last theme user selected and rendering the same next time user visits
+    const [darkTheme,setDarkTheme] = useLocalStorage('darkTheme',false)
 
     return(
         <ThemeContext.Provider value={{darkTheme,setDarkTheme}}>
